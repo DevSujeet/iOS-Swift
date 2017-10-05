9bc759703f6edd82c0be77c1fa7cf80ed2edd9e7
Sujeet Kumar <sujeetk@fractalanalytics.com>
2017-10-05 12:49:50 +0530

    Initial Commit

---
+++ CUCollectionViewTest1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CUCollectionViewTest1
+//
+//  Created by Sujeet.Kumar on 10/5/17.
+//  Copyright © 2017 Fractal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
